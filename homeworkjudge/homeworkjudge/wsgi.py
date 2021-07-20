ce2ca6c35b61135257e4e5eb1d953f381fa61458
RayLin9981 <a259108736@gmail.com>
2021-07-20 13:05:10 +0800

    change Pipfile and wsgi.py

--- homeworkjudge/homeworkjudge/wsgi.py
+++ homeworkjudge/homeworkjudge/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 import sys
-sys.path.append('/var/www/homeworkjudge/1092homeworkjudge/.venv/lib/python3.8/site-packages')
-sys.path.insert(0,'/var/www/homeworkjudge/1092homeworkjudge/.venv/lib/python3.8/site-packages')
-sys.path.append('/var/www/homeworkjudge/1092homeworkjudge/homeworkjudge')
+ssys.path.append('/var/www/pc2webDjango/.venv/lib/python3.6/site-packages')
+sys.path.insert(0,'/var/www/pc2webDjango/.venv/lib/python3.6/site-packages')
+sys.path.append('/var/www/pc2webDjango/homeworkjudge')
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'homeworkjudge.settings')
